217a5b3ae98309998ec6b561fdf7bbe437662b15
m-aljamal <mohammadjamol@gmail.com>
2022-06-02 22:30:24 +0300

    deploy

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ export default function Home() {
   return (
      <div>
        <h1 className='container mx-auto text-5xl'>test</h1>
+       <h1 className='container mx-auto text-5xl'>test</h1>
      </div>
   )
 }
